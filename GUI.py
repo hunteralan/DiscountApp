7905dfc1d24eec1b75417ae91d7e1df01196168e
D Collins <dolphin.dalton@gmail.com>
2022-03-04 11:53:14 -0600

    Clear fields

--- GUI.py
+++ GUI.py
@@ -1,7 +1,8 @@
 import sys
 from PyQt5 import QtWidgets, uic, QtGui, QtCore
 from PyQt5.QtWidgets import QMessageBox
-from Classes.Employee import Employee 
+from Classes.Employee import Employee
+from Classes.Database import Database 
 
 class Ui(QtWidgets.QDialog):
     def __init__(self):
@@ -9,10 +10,10 @@ class Ui(QtWidgets.QDialog):
         uic.loadUi('UI/registration.ui', self)
         
         self.submit = self.findChild(QtWidgets.QPushButton, 'pushButton_2')
-        self.submit.clicked.connect(self.onSubmit)
+        self.submit.clicked.connect(self.onSubmitRegistration)
         
         self.cancel = self.findChild(QtWidgets.QPushButton, 'pushButton')
-        self.cancel.clicked.connect(self.onCancel)
+        self.cancel.clicked.connect(self.onCancelRegistration)
         
         self.username = self.findChild(QtWidgets.QLineEdit, 'lineEdit')
         self.password = self.findChild(QtWidgets.QLineEdit, 'lineEdit_2')
@@ -21,30 +22,45 @@ class Ui(QtWidgets.QDialog):
         
         self.show()
     
-    def onSubmit(self):
+    def onSubmitRegistration(self):
         if (self.password.text() != self.passwordCheck.text()):
             failMsg = QMessageBox()
             failMsg.setIcon(QMessageBox.Critical)
             failMsg.setWindowTitle('Error!')
             failMsg.setText("Your Passwords Must Be The Same!")
-        
+
+            self.lineEdit_2.clear()
+            self.lineEdit_3.clear()
+            
             x = failMsg.exec_()
         else:
-            #print(self.username.text() + '\n')
-            #print(self.password.text() + '\n')
-            #print(self.passwordCheck.text() + '\n')
-            #print(self.name.text() + '\n')
+            try:
+                print(self.username.text() + '\n')
+                print(self.password.text() + '\n')
+                print(self.passwordCheck.text() + '\n')
+                print(self.name.text() + '\n')
             
-            newEmployee = Employee(username=self.username.text(), password=self.password.text(), employeeName=self.name.text())
-            newEmployee.createAccount()
-    
-    def onCancel(self):
+                newEmployee = Employee(username=self.username.text(), password=self.password.text(), employeeName=self.name.text())
+                newEmployee.createAccount()
+                Employee("test").displayTable()
+            
+                self.lineEdit.clear()
+                self.lineEdit_2.clear()
+                self.lineEdit_3.clear()
+                self.lineEdit_4.clear()
+            except Exception:
+                print("Exception here")
+    def onCancelRegistration(self):
         self.close()
 
 
 if __name__ == '__main__':
+    
+    #authDB = Database(dbSelection="auth").initialize()
+    #mainDB = Database(dbSelection="main").initialize()
+    
     app = QtWidgets.QApplication(sys.argv)
     window = Ui()
-    print("hi")
+    
     sys.exit(app.exec_())
     
